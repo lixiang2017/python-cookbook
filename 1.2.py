ba80f7eb4204a8206c052b86a749ccb6dd1de12b
lixiang2017 <laoxing201314@outlook.com>
2018-12-02 17:46:46 +0800

    head + sum(tail) if tail else head

--- 1.2.py
+++ 1.2.py
@@ -1,6 +1,7 @@
-
-
+# 从任意长度的可迭代对象中分解元素
 # 星号表达式
+
+
 def drop_first_last(grades):
     first, *middle, last = grades
     return avg(middle)
@@ -40,3 +41,72 @@ def avg_comparison(a, b):
         return -1
     else:
         return 0
+
+
+*trailing, current = [10, 8, 7, 1, 9, 5, 10, 3]
+print(trailing)
+print(current)
+
+records = [
+    ('foo', 1, 2),
+    ('bar', 'hello'),
+    ('foo', 3, 4)
+]
+
+
+def do_foo(x, y):
+    print('foo', x, y)
+
+
+def do_bar(s):
+    print('bar', s)
+
+
+for tag, *args in records:
+    if tag == 'foo':
+        do_foo(*args)
+    elif tag == 'bar':
+        do_bar(*args)
+
+# cat /etc/shadow | grep nobody
+# nobody:*:17647:0:99999:7:::
+line = 'nobody:*:-2:-2:Unprivileged User:/var/empty:/usr/bin/false'
+uname, *fields, homedir, sh = line.split(':')
+print(uname)
+print(homedir)
+print(sh)
+
+# _ ign ignore
+record = ('ACME', 50, 123.45, (12, 18, 2012))
+name, *_, (*_, year) = record
+print(name)
+print(year)
+
+
+# recursive
+items = [1, 10, 7, 4, 5, 9]
+head, *tail = items
+print(head)
+print(tail)
+
+
+def sum(items):
+    head, *tail = items
+    return head + sum(tail) if tail else head
+
+
+print('sum =', sum(items))               # 36
+'''
+(head + sum(tail)) if tail else head       36
+(head + sum(tail)) if tail else head * 2   45
+head + (sum(tail) if tail else head)       45
+head + (sum(tail) if tail else head * 2)   54
+head + sum(tail) if tail else head * 2     45
+head * 2 + sum(tail) if tail else head     63
+'''
+# 递归出口
+head, *tail = [9]
+print(head)
+print(tail)
+
+
